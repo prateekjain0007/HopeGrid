3fd906178afc2a9067fb564b80791bae9ccd27cf
Prateek Jain <jainprateek2004@gmail.com>
2025-07-12 23:27:18 +0530

    Update App.jsx to use REACT_APP_API_URL

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
   Navigate
 } from 'react-router-dom';
 
-import { Toaster } from 'react-hot-toast'; // Toast support
+import { Toaster } from 'react-hot-toast';
 
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
@@ -20,12 +20,14 @@ import MapView from './pages/MapView';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 function App() {
   const [reports, setReports] = useState([]);
   const isLoggedIn = !!localStorage.getItem('token');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/reports')
+    fetch(`${API_URL}/reports`)
       .then((res) => res.json())
       .then((data) => setReports(data))
       .catch((err) => console.error('Failed to fetch reports:', err));
@@ -59,7 +61,7 @@ function App() {
           },
         }}
       />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
@@ -113,6 +115,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
